e4fd8f14ce3df8ecec33c0f0e63f79786e6efd89
jcsilverx <jcsilverx@gmail.com>
2025-03-12 15:30:10 -0400

    refactor(fp-core): refactor `canonicalizeKeyedCollectionKey` method

--- packages/fp-core/src/keyed-collections/canonicalize-keyed-collection-key.ts
+++ packages/fp-core/src/keyed-collections/canonicalize-keyed-collection-key.ts
@@ -1,5 +1,8 @@
 import { sameValue } from "../comparison-operations";
 
+/**
+ * @since 0.0.16
+ */
 const canonicalizeKeyedCollectionKey = <K>(key: K): K =>
   sameValue(key, -0) ? (+0 as K) : key;
 
