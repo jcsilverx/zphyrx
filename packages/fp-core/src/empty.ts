9f14fc4db557b5531a4edb0a57f31b2e7980288b
jcsilverx <jcsilverx@gmail.com>
2025-03-22 17:32:04 -0400

    feat(fp-core): add `EMPTY` const

---
+++ packages/fp-core/src/empty.ts
@@ -0,0 +1,6 @@
+/**
+ * @since 0.0.17
+ */
+const EMPTY = Symbol("EMPTY");
+
+export { EMPTY };
--- packages/fp-core/src/index.ts
+++ packages/fp-core/src/index.ts
@@ -2,5 +2,6 @@ export * from "./comparison-operations";
 export * from "./keyed-collections";
 export * from "./object";
 export * from "./sort";
+export * from "./empty";
 export * from "./shift";
 export * from "./tag-tester";
